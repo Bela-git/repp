0bfbdbbe9565c89a076684359ef385f1dbd31b0e
Bela <aa0981281601@gmail.com>
2025-03-26 16:44:50 +0800

    部屬

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-# app.py
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+# 根路徑
 @app.route('/')
 def home():
-    return "Hello, Flask!"
+    return 'Hello, Flask!'
+
+# 自定義 API 路徑
+@app.route('/api/data', methods=['GET'])
+def get_data():
+    return jsonify({'name': 'John Doe', 'job': 'Developer'})
 
 if __name__ == '__main__':
     app.run(debug=True)
