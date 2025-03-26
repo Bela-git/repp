05330005702d41a827c749e4bb22f674cfec5a40
Bela <aa0981281601@gmail.com>
2025-03-26 17:46:30 +0800

    1j4gj3

--- app.py
+++ app.py
@@ -2,15 +2,9 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-# 根路徑
-@app.route('/')
-def home():
-    return 'Hello, Flask!'
-
-# 自定義 API 路徑
 @app.route('/api/data', methods=['GET'])
 def get_data():
-    return jsonify({'name': 'John Doe', 'job': 'Developer'})
+    return jsonify({"name": "John", "job": "Developer"})
 
 if __name__ == '__main__':
     app.run(debug=True)
